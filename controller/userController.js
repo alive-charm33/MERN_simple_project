6862accfc15888b91402b58f5e28970653b4ca3c
Sejal <bargalesejal@gmail.com>
2026-07-09 12:37:20 +0530

    Implement HTTP POST API to insert user data into MongoDB

---
+++ controller/userController.js
@@ -0,0 +1,17 @@
+import User from "../model/userModel.js";
+
+
+export const create=async(req,res)=>{
+    try{
+        const newUser=new User(req.body);
+        const {email}=newUser;
+        const userExist=await User.findOne({email});
+        if(userExist){
+            return res.status(400).json({message:"User already exists"});
+        }
+        await newUser.save();
+        res.status(201).json({message:"User created successfully", user: newUser});
+    }catch(error){
+        res.status(500).json({message:"Error creating user",error:error.message});
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-// import routes from './routes';
+import routes from './routes/userRoute.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
@@ -10,9 +10,13 @@ app.use(bodyParser.json());
 // app.use(cors());
 // app.use('/api', routes);
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 8000;
 const MONGO_URL = process.env.MONGO_URL;
+//debug code
+console.log("PORT from .env:", process.env.PORT);
+console.log("MONGO_URL:", process.env.MONGO_URL);
 
+//db
 mongoose
     .connect(MONGO_URL)
     .then(() => {
@@ -24,3 +28,20 @@ mongoose
     .catch((error) => {
         console.log('Error connecting to MongoDB:', error);
     });
+app.get("/", (req, res) => {
+    res.send("Server is working");
+});
+    app.use("/api",routes);
+
+
+// import express from "express";
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// });
+
+// app.listen(8000, () => {
+//     console.log("Listening on 8000");
+// });
