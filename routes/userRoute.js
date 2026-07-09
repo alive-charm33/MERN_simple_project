08b06d9aef87a6be5a1848417eae72a116fd390f
Sejal <bargalesejal@gmail.com>
2026-07-09 16:19:26 +0530

    delete user by ID

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {create,getAllUsers,getuserById,update} from "../controller/userController.js";
+import {create,getAllUsers,getuserById,update,deleteUser} from "../controller/userController.js";
 
 
 const route=express.Router();
@@ -8,5 +8,6 @@ route.post("/user",create);
 route.get("/users",getAllUsers);
 route.get("/user/:id",getuserById);
 route.put("/user/:id",update);
+route.delete("/user/:id",deleteUser);
 
 export default route;
